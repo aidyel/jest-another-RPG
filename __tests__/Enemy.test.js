20d4282c3c2f70bb12df0284eef6cfe065df9455
aidyel <aidayeli@icloud.com>
2022-03-26 11:51:50 -0400

    finished refactoring code

--- __tests__/Enemy.test.js
+++ __tests__/Enemy.test.js
@@ -10,7 +10,7 @@ test('creates an enemy object', () => {
     expect(enemy.name).toBe('goblin');
     expect(enemy.weapon).toBe('sword');
     expect(enemy.health).toEqual(expect.any(Number));
-    expect(enemy.strenth).toEqual(expect.any(Number));
+    expect(enemy.strength).toEqual(expect.any(Number));
     expect(enemy.agility).toEqual(expect.any(Number));
     expect(enemy.potion).toEqual(expect.any(Object));
 
